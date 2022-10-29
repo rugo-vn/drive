d3a456374afbf8d472bffbc6fc688f74bcf387e1
Vu Anh Hao <hi@haova.me>
2022-10-29 19:36:31 +0700

    Add fs schema

---
+++ src/fs/schema.js
@@ -0,0 +1,9 @@
+export const schema = {
+  properties: {
+    name: { type: 'string' },
+    mime: { type: 'string' },
+    parent: { type: 'string' },
+    size: { type: 'number' },
+    updatedAt: { type: 'string' },
+  }
+}
